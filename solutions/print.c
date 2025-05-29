cee4d7188adcef6ce60eb8843e499557dad4893e
Kai Mischke <kai-timo.mischke@stud.thga.de>
2025-05-29 09:23:27 +0200

    Add print.c solution for Task 3

---
+++ solutions/print.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// printf-Ausgabe mit verschiedenen Datentypen
+	printf("Hello from C!\n");
+	printf("Integer: %d, Float: %.2f, String: %s\n", 42, 3.14, "test");
+
+	// puts-Ausgabe 
+	puts ("This is puts output");
+
+	return 0;
+
+}
